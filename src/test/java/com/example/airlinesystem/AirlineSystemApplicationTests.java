891ecebbfaba34e4dcaf1812db483b254f174db4
Lvkoena github <Lvkoena@gmail.com>
2022-09-19 22:20:20 +0200

    first commit

---
+++ src/test/java/com/example/airlinesystem/AirlineSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
